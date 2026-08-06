bde029c37d39eb1256721a5d13d28add4266580c
Davi Faria <dsf19y@gmail.com>
2026-08-06 18:47:28 -0300

    Update 1.c

--- Algoritimos-2/LAB 3 algoritimos 2/1.c
+++ Algoritimos-2/LAB 3 algoritimos 2/1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    int *vetor = (int *)malloc(5 * sizeof(int));
+    if (vetor == NULL) return 1;
+
+    printf("Digite 5 numeros:\n");
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &vetor[i]);
+    }
+    printf("Numeros digitados:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+
+    free(vetor);
+    return 0;
+}
